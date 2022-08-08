ad59b41fc371170ec8450a1a0003d408589dcaf2
jose <joseandrescolmenares02@gmail.com>
2022-08-08 15:07:32 -0300

    avances

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import socket from './Socket'
+import s from './css/Home.module.css'
 
 const  Home = () => {
+
+    const  [nombres, setNombres] = useState([])
+    
+    useEffect(() => {
+        socket.on('nombre', name => {
+            setNombres([...nombres,name])
+          return () => {socket.off()}
+        })
+    },[nombres])
+  
+    
     return (
          <div>
-            soy home
+         {nombres.map(el => <div>{el.name}</div>)}
          </div>
     )
 }
--- src/components/Socket.js
+++ src/components/Socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-let socket = io("//localhost:3001")
+let socket = io("//localhost:3001" || "https://grupo-chat.herokuapp.com/" )
 
 export default socket
