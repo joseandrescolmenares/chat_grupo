af40a30739bac920239d5149aa7e920a27ca3d3e
jose <joseandrescolmenares02@gmail.com>
2022-08-08 14:15:50 -0300

    avances

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import s from './css/Form.module.css'
-
+import socket from './Socket'
+import { useNavigate } from "react-router-dom";
 const Form = () => {
+const [name, setName] = useState('')
+
+let navigate = useNavigate();
+const handleOnsubmit = (e) => {
+    e.preventDefault()
+    socket.emit('conectado', name)
+
+   setTimeout(() =>{
+    navigate("/home")
+   },1500)
+   
+}
  return(
     <div className={s.container}>
     <h1 className={s.h1}>Master chat</h1>
-    <div className={s.form}> 
-         <label>Ingrese su Nombre</label>
-        <input className={s.input}></input>
+    <form className={s.form} onSubmit={handleOnsubmit}> 
+         <label >Ingrese su Nombre</label>
+        <input className={s.input} placeholder='Nombre'  value={name}  type='text' minLength='4' required  onChange={(e) => setName(e.target.value)}></input>
         <button className={s.boton}>Registrar</button>
-        </div>
+        </form>
     </div>
  )
 }
